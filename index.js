75fbbc0a0a267d79de56776f53b48d3b5f63cebe
Sandy Zhang <sandyzhang142@gmail.com>
2022-10-10 01:38:24 -0400

    commit tree 87e6465b09f96501c64c63de39368d6d030bd32a

---
+++ index.js
@@ -0,0 +1,48 @@
+function add () {
+
+}
+
+function subtract() {
+
+}
+function multiply() {
+
+}
+
+function divide() {
+
+}
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+
+function increment(n) {
+    n++;
+    return n;
+
+}
+
+function decrement(n) {
+    n--;
+    return n;
+}
+function makeInt(string){
+    return parseInt(string,10)
+}
+function preserveDecimal(string){
+    return parseFloat(string);
+
+}
